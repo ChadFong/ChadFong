dd17557db620b9a02422eb1f10e4422ac0868aca
ChadFong <ChadMFong@Gmail.com>
2015-07-23 14:03:38 -0700

    (add) navbar functionality
    color scheme not final.
    
    Uses flexboxes to resize dynamically.
    
    Links do not overlap.  There are options for active, hover, and regular

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,10 +4,16 @@ var Link = require('react-router').Link;
 var Navbar = React.createClass({
   render: function(){
     return (
-      <div>
-        <Link to="about">About</Link>
-        <Link to="blog">Blog</Link>
-        <Link to="projects">Projects</Link>
+      <div className="navbar flex-container">
+        <div>
+          <Link to="about">About</Link>
+        </div>
+        <div>
+          <Link to="blog">Blog</Link>
+        </div>
+        <div>
+          <Link to="projects">Projects</Link>
+        </div>
       </div>
     )
   }
