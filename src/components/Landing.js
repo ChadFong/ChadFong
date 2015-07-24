1accc48d7cd7aaa44655b551827287ca1a5178b4
ChadFong <ChadMFong@Gmail.com>
2015-07-24 15:27:03 -0700

    (refactor) photo placement and references

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -4,7 +4,7 @@ var Landing = React.createClass({
   render: function(){
     return (
       <div className="Landing flex-container">
-        <img src="../src/Assets/HeadCir.png" />
+        <img src="./Assets/Photos/HeadCir.png" />
         <h1>Welcome!</h1>
         <h2>My name is Chad, and I am a software developer</h2>
         <h3>Feel free to explore my site to learn more about me</h3>
