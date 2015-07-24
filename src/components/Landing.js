1897dd4e41823852caca34f447ac82e2d4f898ef
ChadFong <ChadMFong@Gmail.com>
2015-07-24 15:04:14 -0700

    (add) landing page implementation
    Style to center, add photo and placeholder text/styling

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -3,8 +3,11 @@ var React = require('react');
 var Landing = React.createClass({
   render: function(){
     return (
-      <div>
-        <h3> Landing Page </h3>
+      <div className="Landing flex-container">
+        <img src="../src/Assets/HeadCir.png" />
+        <h1>Welcome!</h1>
+        <h2>My name is Chad, and I am a software developer</h2>
+        <h3>Feel free to explore my site to learn more about me</h3>
       </div>
     )
   }
