337833d463e8463653c891d31bacf36088cf681f
ChadFong <ChadMFong@Gmail.com>
2015-07-24 17:53:44 -0700

    (add) website profile links to top of about
    + alpha styling

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,21 @@ var About = React.createClass({
   render: function(){
     return (
       <div>
-        <h3> About </h3>
+        <div className="websites flex-container">
+          <p>{"<"}</p>
+          <div>
+            <a href="https://www.linkedin.com/in/chadfong">LinkedIn</a>
+          </div>
+          <div>•</div>
+          <div>
+            <a href="http://stackoverflow.com/users/4543913/">Stack Overflow</a>
+          </div>
+          <div>•</div>
+          <div>
+            <a href="https://github.com/ChadFong">Github</a>
+          </div>
+          <p>{">"}</p>
+        </div>
       </div>
     )
   }
