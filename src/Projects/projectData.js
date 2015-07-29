6ffbf8352a58154f466557c2ff62792769b9f762
ChadFong <ChadMFong@Gmail.com>
2015-07-29 16:33:35 -0700

    (style) add Projects formatting and data insertion/reading
    Adds loadProjects() to Projects.js which loads projects from
      projectData.js
    
    CSS styling for Projects page

---
+++ src/Projects/projectData.js
@@ -0,0 +1,24 @@
+module.exports = [
+  {
+    title: "Marginalio",
+    shortBlurb: "This is a test",
+    screenSrc: "./Assets/Projects/Marginalio",
+    longBlurb: "This is a much longer test",
+    bulletPoints: [
+      "I did a thing",
+      "I did another thing",
+      "Oh, and that other thing too",
+    ]
+  },
+  {
+    title: "Telefrag",
+    shortBlurb: "This is a test",
+    screenSrc: "./Assets/Projects/Marginalio",
+    longBlurb: "This is a much longer test",
+    bulletPoints: [
+      "I did a thing",
+      "I did another thing",
+      "Oh, and that other thing too",
+    ]
+  }
+];
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,10 +1,39 @@
 var React = require('react');
 
+var projects = require('../Projects/projectData');
+
 var Projects = React.createClass({
+  loadProjects: function() {
+    return projects.map(function(project) {
+      return (
+        <div className="Project">
+        <div className="Header">
+          <h2>{project.title}: </h2><p>{project.shortBlurb}</p>
+        </div>
+          <div className="Content flex-container">
+            <div className="MainScreen">
+              <img src={project.screenSrc} />
+            </div>
+            <div className="Description">
+              <p>{project.longBlurb}</p>
+              <ul className="ResumePoints">
+                {project.bulletPoints.map(function(point) {
+                  return (
+                      <li>{point}</li>
+                    )
+                })}
+              </ul>
+            </div>
+          </div>
+        </div>
+        );
+    });
+  },
+
   render: function(){
     return (
-      <div>
-        <h3> Projects </h3>
+      <div className="Projects-Holder">
+        {this.loadProjects()}
       </div>
     )
   }
